a23bfbf528484c70016c953bb38a6271099e82fe
CindoddCindy <cindodcindy@gmail.com>
2021-07-15 10:39:56 +0700

    master

---
+++ belajargitmerge/src/main/java/com/trygitmerge/belajargitmerge/BelajargitmergeApplication.java
@@ -0,0 +1,13 @@
+package com.trygitmerge.belajargitmerge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajargitmergeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajargitmergeApplication.class, args);
+	}
+
+}
